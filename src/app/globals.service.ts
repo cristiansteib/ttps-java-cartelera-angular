6b1fc9cefbc874e961d903ce828e07daee687522
Cristian Steib <cristian.steib@avature.net>
2018-11-30 19:35:25 -0300

    working login

---
+++ src/app/globals.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalsService {
+  getApiUrl(){
+    return "http://127.0.0.1:8080/"
+  }
+  
+}
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { User } from 'src/models/user';
 import { HttpClient } from '@angular/common/http';
+import { GlobalsService } from '../globals.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http: HttpClient) {} 
-
+  constructor(
+    private http: HttpClient,
+    private globalsService: GlobalsService
+    ) {} 
+    url = this.globalsService.getApiUrl()
   /** POST: Login a user*/
   loginUser(user: User) {
-    this.http.post<any>("auth/login", user).subscribe(
+    this.http.post<any>(this.url + "auth/login", user).subscribe(
       data => {
         localStorage.setItem("token", data.token)
         localStorage.setItem("user_id", data.userId)
