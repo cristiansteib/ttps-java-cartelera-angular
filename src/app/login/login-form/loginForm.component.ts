5935527d9910bdd8be7400e5250b06ed8995f647
Cristian Steib <cristiansteib@gmail.com>
2018-11-30 21:27:37 -0300

    s

--- src/app/login/login-form/loginForm.component.ts
+++ src/app/login/login-form/loginForm.component.ts
@@ -13,16 +13,16 @@ import { LoginService } from '../login.service';
    export class LoginFormComponent {
 
     constructor(private loginService: LoginService) {}
-    model = new User(null,'','')
+    user = new User(null,'','')
     submitted = false
     
     onSubmit(formulario: NgForm) {
         if(formulario.valid) {
-            this.model.username = formulario.value.username
-            this.model.password = formulario.value.password
+            this.user.username = formulario.value.username
+            this.user.password = formulario.value.password
             this.submitted = true
-            this.loginService.loginUser(this.model)
+            this.loginService.loginUser(this.user)
         } 
     }
-    get diagnostic() { return JSON.stringify(this.model); }
+    get diagnostic() { return JSON.stringify(this.user); }
   }  
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { User } from 'src/models/user';
 import { HttpClient } from '@angular/common/http';
 import { GlobalsService } from '../globals.service';
@@ -15,13 +17,17 @@ export class LoginService {
     url = this.globalsService.getApiUrl()
   /** POST: Login a user*/
   loginUser(user: User) {
+    const body = new HttpParams()
+    .set('username', user.username)
+    .set('password', user.password);
+    
     this.http.post<any>(this.url + "auth/login", user).subscribe(
       data => {
         localStorage.setItem("token", data.token)
         localStorage.setItem("user_id", data.userId)
-      }
-    )
-  }
+      })
+    }
+  
 
   /** Logout the current user*/
   logoutUser() {
