158c594904bf267f1167a334af44f088d102f3b1
Cristian Steib <cristiansteib@gmail.com>
2018-12-12 22:47:27 -0300

    update

--- src/app/publication/publication.component.ts
+++ src/app/publication/publication.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {PublicationService} from './publication.service';
 import {Publication} from '../../models/Publication';
-import { PublicationComment } from 'src/models/PublicationComment';
+import {PublicationComment} from 'src/models/PublicationComment';
 import {BillboardService} from '../cartelera/billboard.service';
-import { Billboard } from 'src/models/Billboard';
+import {Billboard} from 'src/models/Billboard';
 
 @Component({
   selector: 'app-publication',
@@ -20,7 +20,8 @@ export class PublicationComponent implements OnInit {
     private billboardService: BillboardService,
     private publicationService: PublicationService) {
   }
-  billboardSelected: Billboard
+
+  billboardSelected: Billboard;
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
@@ -28,18 +29,18 @@ export class PublicationComponent implements OnInit {
 
 
       if (params['id_pub']) {
-        this.publicationService.getPublication(params['id_billboard'],params['id_pub']).subscribe(
+        this.publicationService.getPublication(params['id_billboard'], params['id_pub']).subscribe(
           publication => {
-            this.currentPublication = publication;      
+            this.currentPublication = publication;
           }
         );
 
-        this.publicationService.getCommentsAndResponses(params['id_billboard'],params['id_pub']).subscribe(
+        this.publicationService.getCommentsAndResponses(params['id_billboard'], params['id_pub']).subscribe(
           publication => {
             this.currentPublicationComments = publication;
           }
         );
-        
+
       }
     });
   }
