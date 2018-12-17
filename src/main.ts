f2fd23c364ad34dc58eb71ce2e79d467245956bf
Cristian Steib <cristiansteib@gmail.com>
2018-12-17 00:27:45 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+declare var loadMaterilize: any;
 
 if (environment.production) {
   enableProdMode();
 }
+loadMaterilize();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
